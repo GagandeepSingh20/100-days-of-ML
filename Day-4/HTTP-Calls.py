4a4e42e57971348138247a99a8758a7208f90d76
Gagandeep Singh Choudhary <gchoudhary16555@gmail.com>
2026-03-07 21:32:36 +0530

    Update API for patient dataset management

--- Day-4/HTTP-Calls.py
+++ Day-4/HTTP-Calls.py
@@ -4,18 +4,18 @@ import json
 app=FastAPI()
 
 def load_data():
-    with open('palestinian_movies.json','r') as f:
+    with open('patient_dataset.json','r') as f:
         data=json.load(f)
 
     return data
 
 @app.get("/")
 def hello():
-    return{'message': 'API of palestinian movies'}
+    return{'message': 'API of Patients'}
 
 @app.get("/about")
 def about():
-    return {'message':'A fully functional API to manage your movies'}
+    return {'message':'A fully functional API to manage your patient history'}
 
 @app.get("/view")
 def view():
